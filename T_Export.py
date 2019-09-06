6324f48e101a54b4a0244c970d362ef619ad2920
Thamarai-Selvam <sthamarai001@gmail.com>
2019-09-06 14:44:41 +0530

    Uploading app files

---
+++ T_Export.py
@@ -0,0 +1,102 @@
+from telethon import TelegramClient,events,sync
+from hurry.filesize import size
+
+def Reverse(list):
+    list.reverse()
+    return list
+
+def getstickers(list):
+    for msg in list:
+        print(msg.id,msg.message,msg.media.document.attributes[1].alt,
+                            msg.media.document.mime_type,size(msg.media.document.size))
+
+def getimages(list):
+    for msg in list:
+         if(hasattr(msg.media,'document')):
+             print(msg.id,msg.message,msg.media.document.attributes[1].file_name,
+                            msg.media.document.mime_type,size(msg.media.document.size))
+         elif(hasattr(msg.media,'photo')):
+            print(msg.id,msg.message)
+
+def getaudio(list):
+    for msg in list:
+        print("Under construction")
+
+def getothers(list):
+    for msg in list:
+        print(msg.id,msg.message,msg.media.document.attributes[0].file_name,
+                        msg.media.document.mime_type,size(msg.media.document.size))
+
+def gettext(list):
+    for msg in list:
+        print(msg.id,msg.message)
+
+# Use your own values from my.telegram.org
+api_id = '1098468'
+api_hash = '6cd36151df3e26cc2e4780d38a43536e'
+
+# The first parameter is the .session file name (absolute paths allowed)
+
+
+client = TelegramClient('mysession', api_id, api_hash)
+client.start()
+
+#print(client.get_me().stringify())
+
+try:
+    messages = client.get_messages('dr_stone',limit=20000)
+except:
+    print('Channel Not Found/Cannot Connect')
+    
+#print(messages[70])
+
+
+#variables for holding different file types
+#string vars
+ltext =[]
+laudio = []
+lvideo = []
+limages = []
+lstickers = []
+lothers = []
+
+
+
+for i,msg in enumerate(Reverse(messages)):
+    if(msg.media != ''):
+        if(hasattr(msg.media,'document')):
+            if(msg.media.document.mime_type == 'image/webp'):        
+                lstickers.append(msg)
+                   
+            elif((msg.media.document.mime_type == 'image/jpeg') or 
+                        (msg.media.document.mime_type == 'image/png')):
+                    limages.append(msg)
+            else:
+                lothers.append(msg)
+
+        elif(hasattr(msg.media,'photo')):
+            limages.append(msg)
+
+        elif(msg.media == None):
+            ltext.append(msg)
+
+
+#print vars
+
+ptext=['Text Messages','Stickers','Images','Audios','Videos','Other Files']
+plist = [ltext,lstickers,limages,laudio,lvideo,lothers]
+#for x,i in enumerate(plist):
+ #   print(ptext[x])
+  #  for j in i:
+   #     print(j)
+        
+print('Text Messages')
+gettext(ltext)
+print('Images')
+getimages(limages)
+print('Audio')
+getaudio(laudio)
+#print('Videos')
+#getvideos(lvideos)
+print('Other files')
+getothers(lothers)
